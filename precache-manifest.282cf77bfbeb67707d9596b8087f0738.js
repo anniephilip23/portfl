c0227661c2923ff706389c82d84c4593ede6da3a
anniephilip23 <60193502+anniephilip23@users.noreply.github.com>
2020-08-14 00:19:29 +0530

    Add files via upload

---
+++ precache-manifest.282cf77bfbeb67707d9596b8087f0738.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9bcf48e561ea30d1662181c3d60008b",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "28e0653c72c340ceb21e",
+    "url": "/ToDo/static/css/main.5412cd93.chunk.css"
+  },
+  {
+    "revision": "30a712a6912dea92f566",
+    "url": "/ToDo/static/js/2.9f025a28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo/static/js/2.9f025a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e0653c72c340ceb21e",
+    "url": "/ToDo/static/js/main.88ac6890.chunk.js"
+  },
+  {
+    "revision": "54195820d32e0340a99b",
+    "url": "/ToDo/static/js/runtime-main.dcd29f79.js"
+  },
+  {
+    "revision": "dd52ff34e53ae456aa02a9fb747e98a8",
+    "url": "/ToDo/static/media/A.dd52ff34.png"
+  },
+  {
+    "revision": "54320817718de291a73c071cc9d26640",
+    "url": "/ToDo/static/media/abt.54320817.png"
+  },
+  {
+    "revision": "7522e4264f78a6dada04e51f544949e6",
+    "url": "/ToDo/static/media/angularjs.7522e426.png"
+  },
+  {
+    "revision": "f3b0dbc43684f0e9a9a64be3b759a056",
+    "url": "/ToDo/static/media/covid.f3b0dbc4.jpg"
+  },
+  {
+    "revision": "3180a1faad3ccf642ffcbca60471c6b3",
+    "url": "/ToDo/static/media/insure.3180a1fa.jpg"
+  },
+  {
+    "revision": "b502449ea2692788907ff6f342458242",
+    "url": "/ToDo/static/media/js.b502449e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8afa85b9e770ae1c6ab178283ed232c7",
+    "url": "/ToDo/static/media/memory.8afa85b9.png"
+  },
+  {
+    "revision": "9100f2eb47cae949fb1f01853263103c",
+    "url": "/ToDo/static/media/node.9100f2eb.png"
+  },
+  {
+    "revision": "9084ee0f87264199698b25e6c0ea7af6",
+    "url": "/ToDo/static/media/proj.9084ee0f.png"
+  },
+  {
+    "revision": "638272649255f64f39256478c22cad4e",
+    "url": "/ToDo/static/media/quiz.63827264.jpg"
+  },
+  {
+    "revision": "893ec8180b4646ea2d68eb07552f3a2c",
+    "url": "/ToDo/static/media/repo.893ec818.png"
+  },
+  {
+    "revision": "4e4ba1f6f0d10a724515c283a998c536",
+    "url": "/ToDo/static/media/skill.4e4ba1f6.png"
+  },
+  {
+    "revision": "10bff956c61d12ec45e7ce28aa90de0e",
+    "url": "/ToDo/static/media/tictac.10bff956.jpg"
+  },
+  {
+    "revision": "10d0da7ba19ec516dc7f5f4864ecbb86",
+    "url": "/ToDo/static/media/todo.10d0da7b.jpg"
+  }
+]);
